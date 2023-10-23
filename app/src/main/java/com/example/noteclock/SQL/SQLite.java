5f25556028e6404608ca87475546ae64394b04a5
LAPTOP-CDUNFSVS\ADMIN <dacvucat294@gmail.com>
2023-10-23 20:16:01 +0700

    update

--- app/src/main/java/com/example/noteclock/SQL/SQLite.java
+++ app/src/main/java/com/example/noteclock/SQL/SQLite.java
@@ -1,24 +1,101 @@
 package com.example.noteclock.SQL;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
-public class SQLite extends SQLiteOpenHelper {
+import com.example.noteclock.model.Note;
 
-    public SQLite(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+import java.util.ArrayList;
+import java.util.List;
+
+public class SQLite extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "NoteClock.db";
+    private static int DATBASE_VERSION = 1;
+    public SQLite(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATBASE_VERSION);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE notes(" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "title TEXT," + "" +
+                "content TEXT," +
+                "date TEXT," +
+                "time TEXT)";
+        db.execSQL(sql);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS notes");
+        onCreate(sqLiteDatabase);
+    }
+    //Lấy danh sách thông tin
+    public List<Note> getAll(){
+        List<Note> list=new ArrayList<>();
+        SQLiteDatabase st=getReadableDatabase();
+        String order="date DESC";
+        Cursor rs=st.query("notes",null,null,null,null,null,order);
+        while (rs!=null && rs.moveToNext()){
+            int id=rs.getInt(0);
+            String title=rs.getString(1);
+            String content=rs.getString(2);
+            String date=rs.getString(3);
+            String time=rs.getString(4);
+            list.add(new Note(id,title,content,date,time));
+        }
+        return list;
+    }
+    //Thêm thông tin
+    public long Add(Note note){
+        ContentValues values=new ContentValues();
+        values.put("title",note.getTitle());
+        values.put("content",note.getContent());
+        values.put("date",note.getDate());
+        values.put("time",note.getTime());
+        SQLiteDatabase sqLiteDatabase=getWritableDatabase();
+        return sqLiteDatabase.insert("notes",null,values);
+    }
+    //lấy thông tin thoe ngày hiện tại
+    public List<Note> getBydate(String date){
+        List<Note> list=new ArrayList<>();
+        String item="date like ?";
+        String[] whereArgs={date};
+        SQLiteDatabase st=getReadableDatabase();
+        Cursor rs=st.query("notes",null,item,whereArgs,null,null,null);
+        while (rs!=null && rs.moveToNext()){
+            int id=rs.getInt(0);
+            String title=rs.getString(1);
+            String content=rs.getString(2);
+            String date1=rs.getString(3);
+            String time=rs.getString(4);
+            list.add(new Note(id,title,content,date1,time));
+        }
+        return list;
+    }
+    //Chỉnh sửa thông tin
+    public int Update(Note note){
+        ContentValues values=new ContentValues();
+        values.put("title",note.getTitle());
+        values.put("content",note.getContent());
+        values.put("date",note.getDate());
+        values.put("time",note.getTime());
+        SQLiteDatabase sqLiteDatabase=getWritableDatabase();
+        String item="id= ?";
+        String[] whereArgs={Integer.toString(note.getId())};
+        return sqLiteDatabase.update("notes",values,item,whereArgs);
+    }
+    //Xóa thông tin
+    public int Delete(int id){
+        String item="id= ?";
+        String[] whereArgs={Integer.toString(id)};
+        SQLiteDatabase sqLiteDatabase=getWritableDatabase();
+        return sqLiteDatabase.delete("notes",item,whereArgs);
     }
 }
--- app/src/main/java/com/example/noteclock/fragment/FragmentHome.java
+++ app/src/main/java/com/example/noteclock/fragment/FragmentHome.java
@@ -1,22 +1,68 @@
 package com.example.noteclock.fragment;
 
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 
-
+import com.example.noteclock.Adapter.RecycleViewAdapter;
 import com.example.noteclock.R;
+import com.example.noteclock.SQL.SQLite;
+import com.example.noteclock.model.Note;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.Calendar;
+import java.util.List;
 
 
 public class FragmentHome extends Fragment {
+    private RecycleViewAdapter adapter;
+    private TextView tvCount;
+    private RecyclerView recyclerView;
+    private SQLite db;
+    private Calendar myCalendar = Calendar.getInstance();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        recyclerView = view.findViewById(R.id.rcv_list);
+        tvCount = view.findViewById(R.id.tvcount);
+        adapter = new RecycleViewAdapter();
+        db = new SQLite(getContext());
+        List<Note> list = db.getAll();
+        adapter.setNotes(list);
+        LinearLayoutManager manager = new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false);
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setAdapter(adapter);
+        adapter.setItemListener((RecycleViewAdapter.ItemListener) this);
+    }
+
+   
 }
